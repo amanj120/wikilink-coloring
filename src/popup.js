0b997b9b97960557f10db98091c4183a750c86ae
Aman Jain <aman@mac.local>
2022-09-25 13:26:29 -0400

    Added web scraper logic for page views

--- src/popup.js
+++ src/popup.js
@@ -1,38 +1,79 @@
 let actionButton = document.getElementById("actionButton");
 
 actionButton.addEventListener("click", async () => {
+	actionButton.disabled = true;
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
       	target: { tabId: tab.id },
       	func: highlightWikiLinks,
     });
-  });
-  
-// The body of this function will be executed as a content script inside the
-// current page
+});
+
+// main business logic goes here
 function highlightWikiLinks() {
 
-	function isWikiArticle() {
-		// This regex identifies if a page is a Wikipedia article.
-		let wikiDomainPattern = /wikipedia\.org/;
-		return wikiDomainPattern.test(window.location.hostname);
+	// returns the number of views a wiki page 
+	async function getPageViews(url) {
+		let infoUrl = url.replace("/wiki/", "/w/index.php?title=").concat("&action=info");
+		const response = await fetch(infoUrl);
+		const data = await response.text();
+		let tmp = data.match(/<div class="mw-pvi-month">.*<\/div>/)[0]
+			.replace("<div class=\"mw-pvi-month\">", "")
+			.replace("</div>", "");
+		let pageViews = parseInt(tmp.replaceAll(",", ""));
+		return pageViews;
 	}
 
-	if (!isWikiArticle()) {
-		console.log("The current page is not a Wikipedia article");
-		return;
+	// assigns a color to the number of views based on a logarithmic scale
+	function getColorFromPageViews(pageViews) {
+		if (pageViews < 128) {
+			pageViews = 128;
+		} else if (pageViews > 1048576) {
+			pageViews = 1048576;
+		}
+		let strength = Math.floor(((Math.log2(pageViews) - 7) * 511.0)/13.0); 
+			
+		if (strength < 256) {
+			return 'rgb(255, ' + strength + ', 0)';
+		} else {
+			return 'rgb(' + (511-strength) + ', 255, 0)';
+		}
+	}
+
+	if (/wikipedia\.org/.test(window.location.hostname) == false) {
+		return; // The extension only works on wikipedia pages
 	} 
 	
-	let urls = document.getElementById("bodyContent").getElementsByTagName("a")
+	const urls = document.getElementById("bodyContent").getElementsByTagName("a")
+	const urlMap = new Map();
 
-	for (index in urls) {
-		let url = urls.item(index);
-		if (/\/wiki\//.test(url.href) == true) {
-			if (/(File:|Help:|Category:|#)/.test(url.href) == false){
-				url.style.backgroundColor = "#FF00FF";
+	// TODO: filter to include links with "#" that aren't the same page
+	for (let url of urls) {
+		if (/\/wiki\/[^:#]*$/.test(url.href) == true) {
+			let urlHref = url.href;
+			if (urlMap.has(urlHref) == false) {
+				urlMap.set(urlHref, new Array());
 			}
+			urlMap.get(urlHref).push(url);
 		}
-		console.log(url.href);
 	}
-}
 
+	const urlMapEntries = urlMap.entries();
+	const linkColorId = setInterval(() => {
+		let entry = urlMapEntries.next();
+		if (entry.done == true) {
+			clearInterval(linkColorId);
+		} else {
+			let href = entry.value[0];
+			let tags = entry.value[1];
+
+			getPageViews(href).then((pageViews) => {
+				let color = getColorFromPageViews(pageViews);
+				for (let tag of tags) {
+					tag.style.backgroundColor = color;
+				}
+				console.log(href, pageViews);
+			});
+		}
+	}, 50);
+}
